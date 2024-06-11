acf99d7b58a98eabf9ea87a7abec3e3a59ddacb2
MinhHung2703 <nguyenminhhung27032002@gmail.com>
2024-06-11 20:32:40 +0700

    fixing upload image

--- client/src/utils/generateImageURL.js
+++ client/src/utils/generateImageURL.js
@@ -1,18 +1,12 @@
 import axios from "axios";
 
 const generateImageURL = async (image) => {
-  const publicId = "";
-  const cloudName = "";
-  const uploadPreset = "";
-
-  const cld = new Cloudinary({})
-
   const file = new FormData();
   file.append("file", image);
   file.append("upload_preset", "ml_default");
 
   const { data } = await axios.post(
-    `https://api.cloudinary.com/v1_1/495528856755222:yrLPuSK_snTCKawoJ9XhcqtbK8Y@dt6k5lgow/image/upload`,
+    `https://api.cloudinary.com/v1_1/dkrzz3lpw/image/upload`,
     file
   );
   return data;
