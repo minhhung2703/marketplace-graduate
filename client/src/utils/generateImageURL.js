f1b309998b088d517764df39cd8ee47bf7a767e1
MinhHung2703 <nguyenminhhung27032002@gmail.com>
2024-07-11 16:09:02 +0700

    .

--- client/src/utils/generateImageURL.js
+++ client/src/utils/generateImageURL.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const generateImageURL = async (image) => {
   const file = new FormData();
   file.append("file", image);
-  file.append("upload_preset", "graduate"); // ml_default is the CLOUDINARY_PRESET
+  file.append("upload_preset", "graduate"); // graduate is the CLOUDINARY_PRESET
 
   const { data } = await axios.post(
     `https://api.cloudinary.com/v1_1/dt6k5lgow/upload`,
